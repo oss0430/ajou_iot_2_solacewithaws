9108f96846e506ee20283dccfb9dbb2892461d8c
oss0430 <82088950+oss0430@users.noreply.github.com>
2022-10-17 16:25:39 +0900

    Added AWS Config Class now we read setting from json

--- src/cloud_config.py
+++ src/cloud_config.py
@@ -1,7 +1,8 @@
 import json
+import AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 
 class SolaceMQTTConfig():
-    
+
     def __init__(
         self,
         url = None,
@@ -10,46 +11,99 @@ class SolaceMQTTConfig():
         password = None,
         topic = None
     ):
-        
+
         self.url = url
         self.port = port
         self.username = username
         self.password = password
         self.topic = topic
-        
-        
+
+
     def read_config_json(
         self,
         path
     ):
         json_file = open(path)
-        
+
         solace_data_dict = json.load(json_file)
-            
+
         self.username = solace_data_dict['Username']
         self.password = solace_data_dict['Password']
-        
+
         ## NOTE in python using url is different
         ## if url is 'ssl://myurl.messaging.solace.cloud:8883'
         ## client.connect(myurl.messaging.solace.cloud, port = 8883)
-        
+
         full_url = solace_data_dict['Secured MQTT Host']
-            
+
         url  = full_url[6:-5]
         port = int(full_url[-4:])
-            
+
         self.url = url
         self.port = port
-        
-        
-        
+
+
+
     def to_dict(
         self
     ):
-        
+
         return {
             'url':self.url,
             'port':self.port,
             'username':self.username,
             'password':self.password
+        }
+
+
+class AWSMQTTConfig():
+
+    def __init__(
+        self,
+        thing_name = None,
+        host_name = None,
+        root_ca_path = None,
+        private_key_path = None,
+        cert_file_path = None,
+        discnonnection_timeout = None,
+        mqtt_operation_timeout = None
+    ):
+
+        self.thing_name = thing_name
+        self.host_name = host_name
+        self.root_ca_path = root_ca_path
+        self.private_key_path = private_key_path
+        self.cert_file_path = cert_file_path
+        self.disconnection_timeout = disconnection_timeout
+        self.mqtt_operation_timeout = mqtt_operation_timeout
+
+
+    def read_config_json(
+        self,
+        path
+    ):
+        json_file = open(path)
+
+        aws_data_dict = json.load(json_file)
+
+        self.thing_name = aws_data_dict['Thing_Name']
+        self.host_name = aws_data_dict['Host_Name']
+        self.root_ca_path = aws_data_dict['Root_CA']
+        self.private_key_path = aws_data_dict['Prviate_Key']
+        self.cert_file_path = aws_data_dict['Cert_File']
+
+
+
+    def to_dict(
+        self
+    ):
+
+        return {
+            'thing_name': self.thing_name,
+            'host_name': self.host_name,
+            'root_ca_path': self.root_ca_path,
+            'private_key_path': self.private_key_path,
+            'cert_file_path': self.cert_file_path,
+            'disconnection_timeout': self.disconnection_timeout,
+            'mqtt_operation_timeout': self.mqtt_operation_timeout
         }
--- src/device_1.py
+++ src/device_1.py
@@ -4,39 +4,46 @@ import paho.mqtt.client as mqtt
 import RPi.GPIO as gpio
 import struct
 import json
-from light_sensor import MySPIDevice 
-from cloud_config import SolaceMQTTConfig 
+from light_sensor import MySPIDevice
+from cloud_config import SolaceMQTTConfig
 
 """
 Solace publisher
 AWS IoT subscriber
 """
 
+def aws_subscribe():
+    ## Configuration
+    config_disconnection_timeout = 10
+    config_mqtt_operation_timeout = 5
+    aws_config_path = 'aws_config.json'
+    
+
 def main():
-    ## Configuration    
+    ## Configuration
     config_max_speed_hz = 1000000
     solace_topic = 'assignment_2'
     channel = 1
     path = 'solace_config.json'
-    
+
     solaceSetting = SolaceMQTTConfig()
     solaceSetting.read_config_json(path)
     solaceSetting_dict = solaceSetting.to_dict()
-    
-    print(solaceSetting_dict)  
-    
+
+    print(solaceSetting_dict)
+
     device_1 = MySPIDevice()
     device_1.spi.max_speed_hz = config_max_speed_hz
-    
+
     print("Setting...")
-    
+
     ## Connect device to solace MQTT broker
     client = mqtt.Client('device_1')
     client.username_pw_set(username=solaceSetting_dict['username'], password=solaceSetting_dict['password'])
     client.tls_set(ca_certs=certifi.where())
     client.connect(solaceSetting_dict['url'],port=solaceSetting_dict['port'])
-    
-    
+
+
     print("Start Publishing")
     ## Publish Message
     while True:
@@ -45,12 +52,12 @@ def main():
         payload_dict = {'light':light_value}
         payload = json.dumps(payload_dict)
         print(payload)
-        
+
         ## Publish with Payload
         client.publish(solace_topic, payload=payload)
-        
+
         time.sleep(1)
-        
+
 
 if __name__ == '__main__':
     main()
