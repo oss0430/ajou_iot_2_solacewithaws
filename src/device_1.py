cf5428abc56fdb49bdc34eba37a374d7d86fed58
Donghoon Lee <oss0430@github.com>
2022-10-18 17:02:18 +0900

    Addressed Naming issue

--- src/device_1.py
+++ src/device_1.py
@@ -18,7 +18,7 @@ def main():
     config_max_speed_hz = 1000000
     solace_topic = 'assignment_2'
     channel = 1
-    path = 'solace_config.json'
+    solace_config_path = 'solace_config.json'
 
     config_disconnection_timeout = 10
     config_mqtt_operation_timeout = 5
@@ -27,7 +27,7 @@ def main():
     aws_topic = 'assignment_2'
 
     solaceSetting = SolaceMQTTConfig()
-    solaceSetting.read_config_json(path)
+    solaceSetting.read_config_json(solace_config_path)
     solaceSetting_dict = solaceSetting.to_dict()
     print(solaceSetting_dict)
 
@@ -43,23 +43,23 @@ def main():
     print("Setting...")
 
     ## Connect device to solace MQTT broker
-    client = mqtt.Client(client_id)
-    client.username_pw_set(username=solaceSetting_dict['username'], password=solaceSetting_dict['password'])
-    client.tls_set(ca_certs=certifi.where())
-    client.connect(solaceSetting_dict['url'],port=solaceSetting_dict['port'])
+    solace_client = mqtt.Client(client_id)
+    solace_client.username_pw_set(username=solaceSetting_dict['username'], password=solaceSetting_dict['password'])
+    solace_client.tls_set(ca_certs=certifi.where())
+    solace_client.connect(solaceSetting_dict['url'],port=solaceSetting_dict['port'])
     
 
     ## Connect and Subscribe to AWS MQTT broker
-    Client = AWSIoTPyMQTT.AWSIoTMQTTClient(client_id)
-    Client.configureEndpoint(awsSetting_dict['host_name'], 8883) 
-    Client.configureCredentials(awsSetting_dict['root_ca_path'], awsSetting_dict['private_key_path'], awsSetting_dict['cert_file_path']) 
-    Client.configureConnectDisconnectTimeout(config_disconnection_timeout) 
-    Client.configureMQTTOperationTimeout(config_mqtt_operation_timeout)
+    aws_client = AWSIoTPyMQTT.AWSIoTMQTTClient(client_id)
+    aws_client.configureEndpoint(awsSetting_dict['host_name'], 8883) 
+    aws_client.configureCredentials(awsSetting_dict['root_ca_path'], awsSetting_dict['private_key_path'], awsSetting_dict['cert_file_path']) 
+    aws_client.configureConnectDisconnectTimeout(config_disconnection_timeout) 
+    aws_client.configureMQTTOperationTimeout(config_mqtt_operation_timeout)
     
     def awsConnectCallback(mid, data):
         print("AWS connected")
     
-    Client.connectAsync(ackCallback=awsConnectCallback)
+    aws_client.connectAsync(ackCallback=awsConnectCallback)
 
     def awsSubscribeCallback(mid, data):
         print("AWS Subscribed")
@@ -69,10 +69,10 @@ def main():
         print('message recieved')
         message_to_picture(message, 'received_picture.png')
     
-    Client.subscribeAsync(aws_topic, 1, ackCallback = awsSubscribeCallback, messageCallback = callbackonAWSMessage)
+    aws_client.subscribeAsync(aws_topic, 1, ackCallback = awsSubscribeCallback, messageCallback = callbackonAWSMessage)
 
     
-    ## Publish Message
+    ## Publish Message with solace client
     print("Start Publishing")
     while True:
         ## Get integer value of light
@@ -82,7 +82,7 @@ def main():
         print(payload)
 
         ## Publish with Payload
-        client.publish(solace_topic, payload=payload)
+        solace_client.publish(solace_topic, payload=payload)
 
         time.sleep(1)
 
--- src/device_2.py
+++ src/device_2.py
@@ -1,9 +1,7 @@
 import certifi
 import time
 import paho.mqtt.client as mqtt
-import paho.mqtt.subscribe as subscribe
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
-import struct
 import json
 import picamera
 from cloud_config import SolaceMQTTConfig, AWSMQTTConfig
@@ -44,14 +42,16 @@ def on_message(
 def main():
     ## Configuration    
     solace_topic = 'assignment_2'
-    channel = 1
-    path = 'solace_config.json'
+
+    solace_path = 'solace_config.json'
     aws_config_path = 'aws_config.json'
+
     config_disconnection_timeout = 10
     config_mqtt_operation_timeout = 5
+
     client_id = 'device_2'
     solaceSetting = SolaceMQTTConfig()
-    solaceSetting.read_config_json(path)
+    solaceSetting.read_config_json(solace_path)
     solaceSetting_dict = solaceSetting.to_dict()
     
     print(solaceSetting_dict)  
@@ -65,27 +65,27 @@ def main():
     print("Setting...")
     
     ## Connect device to solace MQTT broker
-    client = mqtt.Client(client_id)
-    client.username_pw_set(username=solaceSetting_dict['username'], password=solaceSetting_dict['password'])
+    solace_client = mqtt.Client(client_id)
+    solace_client.username_pw_set(username=solaceSetting_dict['username'], password=solaceSetting_dict['password'])
     
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.tls_set(ca_certs=certifi.where())
+    solace_client.on_connect = on_connect
+    solace_client.on_message = on_message
+    solace_client.tls_set(ca_certs=certifi.where())
 
-    client.connect(solaceSetting_dict['url'],solaceSetting_dict['port'])
+    solace_client.connect(solaceSetting_dict['url'],solaceSetting_dict['port'])
 
     
     ## Connect and Publish to AWS MQTT broker
-    Client = AWSIoTPyMQTT.AWSIoTMQTTClient(client_id)
-    Client.configureEndpoint(awsSetting_dict['host_name'], 8883) 
-    Client.configureCredentials(awsSetting_dict['root_ca_path'], awsSetting_dict['private_key_path'], awsSetting_dict['cert_file_path']) 
-    Client.configureConnectDisconnectTimeout(config_disconnection_timeout) 
-    Client.configureMQTTOperationTimeout(config_mqtt_operation_timeout)
+    aws_client = AWSIoTPyMQTT.AWSIoTMQTTClient(client_id)
+    aws_client.configureEndpoint(awsSetting_dict['host_name'], 8883) 
+    aws_client.configureCredentials(awsSetting_dict['root_ca_path'], awsSetting_dict['private_key_path'], awsSetting_dict['cert_file_path']) 
+    aws_client.configureConnectDisconnectTimeout(config_disconnection_timeout) 
+    aws_client.configureMQTTOperationTimeout(config_mqtt_operation_timeout)
     
     def awsConnectCallback(mid, data):
         print("AWS connected")
     
-    Client.connectAsync(ackCallback=awsConnectCallback)
+    aws_client.connectAsync(ackCallback=awsConnectCallback)
 
     def awsPublishcallback(mid):
         print("AWS Publish")
@@ -107,9 +107,9 @@ def main():
         
         filename = "picamera_liluminace.png"
         messageJson = picture_to_bytes(filename)
-        Client.publishAsync(aws_topic, messageJson, 1, ackCallback=awsPublishcallback)
+        aws_client.publishAsync(aws_topic, messageJson, 1, ackCallback=awsPublishcallback)
     
-    client.loop_forever()
+    solace_client.loop_forever()
 
 if __name__ == '__main__':
     main()
